11f6d198618602adcbd34d29878580912159d04a
lixiuling <lxling@dotfashion.com>
2018-08-15 16:06:55 +0800

    demo1

--- src/demo1/index.js
+++ src/demo1/index.js
@@ -1,33 +1,64 @@
-import './less/style.less';
+import _ from 'lodash';
+require('./less/style.less');
 import Img from './images/blend/blend7.jpg';
 import Data from './data/data.xml';
 import printMe from './print.js';
 
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-        var element = document.createElement('div');
-        var btn = document.createElement('button');
+function component(){
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
+    var br = document.createElement('br');
 
-        element.innerHTML = _.join(['Hello ', 'webpack'], '');
-        btn.innerHTML = "Click me and check the console!";
+    element.innerHTML = _.join(['Hello ', 'webpack'], '');
+    btn.innerHTML = "Click me and check the console!";
 
-        btn.onclick = printMe;
-        element.appendChild(btn)
+    element.appendChild(btn);
+    element.appendChild(br);
+    element.classList.add('hello');
 
-        element.classList.add('hello');
+    btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(moudle => {
+        var print = moudle.default;
+        print()
+    })
 
-        var myImg = new Image();
-        myImg.src = Img;
-        element.appendChild(myImg);
+    var myImg = new Image();
+    myImg.src = Img;
+    element.appendChild(myImg);
 
-        return element;
-
-    }).catch(error => 'An error occurred while loading the component');
+    return element;
 }
 
-getComponent().then(component => {
-document.body.appendChild(component);
-})
+document.body.appendChild(component());
+
+
+
+// function getComponent() {
+//     return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
+//         var element = document.createElement('div');
+//         var btn = document.createElement('button');
+
+//         element.innerHTML = _.join(['Hello ', 'webpack'], '');
+//         btn.innerHTML = "Click me and check the console!";
+
+//         btn.onclick = printMe;
+//         element.appendChild(btn)
+
+//         element.classList.add('hello');
+
+//         var myImg = new Image();
+//         myImg.src = Img;
+//         element.appendChild(myImg);
+
+//         return element;
+
+//     }).catch(error => 'An error occurred while loading the component');
+// }
+
+// getComponent().then(component => {
+// document.body.appendChild(component);
+// })
+
+
 
 // if(module.hot){
 //     module.hot.accept('./print.js', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ let autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+
 module.exports = {
     entry: {
         demo1: path.resolve(__dirname, 'src/demo1/index.js')
@@ -16,34 +17,36 @@ module.exports = {
         port: 3000,
     },
     plugins: [
+        //new webpack.optimize.UglifyJsPlugin(),//webpack4+废弃 生产模式会自动压缩
         new HtmlWebpackPlugin({
             title: 'Code Splitting'
         }),
+        // new webpack.optimize.CommonsChunkPlugin({ //webpack4+废弃 改用optimization代替
+        //     name: 'manifest'
+        // }),
         new CleanWebpackPlugin(['dist/demo1']),
         new ExtractTextPlugin('[name].css'),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ],
+    optimization: {
+        splitChunks: {
+            name: 'manifest'
+        }
+    },
     output: {
         path: path.resolve(__dirname, 'dist/demo1'),
-        filename: 'bundle-[hash].js',
+        filename: '[name].[hash].js', //[name].[chunkhash].js webpack4+不能用
         chunkFilename: '[name].bundle.js',
         publicPath: '/'
     },
-    
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: ['css-loader', 'postcss-loader']
-                //publicPath:'../' //解决css背景图的路径问题
-            })
-        },{
             test: /\.less$/,
             use: ExtractTextPlugin.extract({
                 fallback: 'style-loader',
-                use: ['css-loader', 'less-loader']
+                use: ['css-loader', 'less-loader', 'postcss-loader'],
+                //publicPath:'../' //解决css背景图的路径问题
             })
         },{
             test: /\.(png|svg|jpg|gif|jpeg)$/,
